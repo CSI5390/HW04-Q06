e47272fe060d78055236ebf7b1b00f547077117f
John Machado <jrmachado@electro-matic.com>
2021-08-11 14:03:34 -0400

    initial commit. project created

---
+++ Machado_Q05_Netbeans/src/machado_q05_netbeans/Machado_Q05_Netbeans.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machado_q05_netbeans;
+
+/**
+ *
+ * @author jrmachado
+ */
+public class Machado_Q05_Netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
